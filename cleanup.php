559922e0615192a85a04ca3acd7c6ccd8f5b15d0
Ke1pl <2keipl@gmail.com>
2025-01-25 09:57:17 -0600

    Added code for saving images from Wayback Machine

---
+++ cleanup.php
@@ -0,0 +1,74 @@
+<?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Happy? Happy!</h1>
+
+<a href='grabber.php?t=<?php echo time(); ?>'>Try grab new images</a><br/>
+<a href='grabber_from_webarchive.php?t=<?php echo time(); ?>'>Try grab old images from web archive</a>
+<?php
+
+include 'helpers.php';
+
+$files1 = get_files_with_meta_data('saved/');
+$files2 = get_files_with_meta_data('saved2/');
+$files3 = get_files_with_meta_data('saved3/');
+$files4 = get_files_with_meta_data('saved4/');
+
+$files = (object) array_merge((array) $files1, (array) $files2, (array) $files3, (array) $files4);
+
+print ('<p>Processed url_params: '.count(array_keys((array)$files)).'</p>');
+
+$files_grouped_by_date = (object)null;
+
+foreach ($files as $file) {
+	$date_for_separator = ($file->{'url_param_type'} == 1) ? url_param_to_date($file->{'url_param'}, 'd-m-Y') : url_param_to_date2($file->{'url_param'}, 'd-m-Y');
+
+	if (property_exists($files_grouped_by_date, $date_for_separator)) {
+    	array_push($files_grouped_by_date->{$date_for_separator}, $file);
+    } else {
+    	$files_grouped_by_date->{$date_for_separator} = [$file];
+    };
+}
+
+$size = 0;
+$not_null_count = 0;
+
+foreach($files_grouped_by_date as $key => $value) {
+	if (count($value)>0) { //TODO remove?
+    	$output = [];
+    	foreach ($value as $file) {
+        	$filename = $file->{'path'};
+        	$file_size = $file->{'size'};
+        	if ($file_size <> 0) {
+				$date = url_param_to_date($file->{'url_param'}, 'd-m-Y H:i');
+				$size += $file_size;
+				$size_readable = human_filesize($file_size);
+				$not_null_count++;
+            
+    			array_push($output, '<a href="'.$filename.'"><img loading="lazy" src="'.$filename.'" width="150" height="100" title="'.$date.' '.$size_readable.'"></a>');
+            }
+    	}
+    	print '<p><b>'.$key.'</b> '.count($output).' unique images of '.count($value).' downloaded</p>';
+    
+    	foreach ($output as $line) {
+        	print $line;	
+        }
+    	
+    	print '<br/>';
+    }
+}
+
+$z = human_filesize($size);
+print '<p>Storage space taken: '.$z.'</p>';
+print '<p>Non empty images: '.$not_null_count.'</p>';
+?>
+</body>
+</html>
